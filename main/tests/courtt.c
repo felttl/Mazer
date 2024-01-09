0db354ee8761bf6fe516ed8cd2084001bd4303e8
ultratavor <felixtonthat64@gmail.com>
2024-01-09 18:37:14 +0100

    teste de debuggage sur l'itineraire

--- main/tests/courtt.c
+++ main/tests/courtt.c
@@ -1,17 +1,57 @@
+#include "../add_lib.h"
 #include "../display.h"
-
+#include <math.h>
+#include "../load.h"
 typedef struct Pion{
     int line;
     int column;
     int num_step;
+    char character;
     struct Pion * suivant;
 } Pion ;
+int get_number_borders(int line, int col){
+    return 2*(line+col-4);
+}
+int short_vec_point(int currentx,int currenty,int sx,int sy){
+    return (int)(abs(currentx-sx)+abs(currenty-sy));
+}
+int min(int*list, int size){
+    int index=-1;
+    if (size > 0){
+        index=0;
+        for (int i=1;i<size;i++){
+            if (list[i] < list[0]){
+                index=i;
+            }
+        }
+    } else {
+        printf("la taille ne peut pas être 0 !\n");
+        exit(EXIT_FAILURE);
+    }
+    return index;
+}
+int minr(int*list, int size){
+    int index=-1;
+    if (size > 0){
+        index=0;
+        for (int i=size-1;i>0;i--){
+            if (list[i] < list[0]){
+                index=i;
+            }
+        }
+    } else {
+        printf("la taille ne peut pas être 0 !\n");
+        exit(EXIT_FAILURE);
+    }
+    return index;
+}
 Pion * create_Pion(int x, int y, int num){
     Pion * res=(Pion*)malloc(sizeof(Pion));
     res->line = x;
     res->column = y;
     res->num_step = num;
     res->suivant = NULL;
+    res->character = '\0';    
     return res;
 }
 void free_pion_chain(Pion * head){
@@ -46,7 +86,7 @@ Pion * display_chain(Pion * head){
     cursor=head;
     // parcours des points
     while(cursor != NULL){
-        printf("(%d,%d)\n", cursor->line, cursor->column);
+        printf("(%d,%d)c=(%c)\n", cursor->line, cursor->column, cursor->character);
         cursor=cursor->suivant;
     }
     printf("\n");
@@ -86,17 +126,17 @@ Pion*shortest_point_way(char**matrix,int ex,int ey,int sx, int sy, int lenx,int 
         //     printf("%c%c\n %c\n",matrix[scanx][scany-1], matrix[scanx][scany-1], matrix[scanx][scany+1]);
         // }
         // on regarde l'index du min pour se déplacer
-        togo=min(nsew, 4);
+        togo = step%2 ? min(nsew, 4) : minr(nsew, 4); // 1/2 etape au cas ou 2 cas ont la même valeur
         if (togo==0){
             scanx--; // on va au nord
         } else if (togo==1){
             scanx++; // on va au sud etc...
         } else if (togo==2){
-            scany++;
-        } else if (togo==3){
             scany--;
+        } else if (togo==3){
+            scany++;
         } else {
-            printf("erreur: max n'est pas trouvé case : (%d,%d)", scanx, scany);
+            printf("erreur: min n'est pas trouvé case : (%d,%d)", scanx, scany);
             exit(EXIT_FAILURE);
         }    
         // en dehors de la matrice pb de caractère
@@ -106,6 +146,7 @@ Pion*shortest_point_way(char**matrix,int ex,int ey,int sx, int sy, int lenx,int 
             maxoccur=3;            
         }
         etape->suivant=create_Pion(scanx, scany, step);
+        etape->character=matrix[scanx][scany];
         etape=etape->suivant;
         step++;
         maxoccur--;      
@@ -121,7 +162,7 @@ Pion*shortest_point_way(char**matrix,int ex,int ey,int sx, int sy, int lenx,int 
 int main(){
     int x=0;
     int y=0;
-    char filename[strlen(filename)]="test5ct.txt";
+    char filename[20]="test5ct.txt";
     char**matrix = get_char_array_fromfile(filename, &x, &y);
 
     printf("premier affichage :\n");
@@ -139,7 +180,7 @@ int main(){
             }
         }
     }
-    Pion*chemin=add_path(forward_right(matrix, ex, ey, x, y), matrix);
+    Pion*chemin=add_path(shortest_point_way(matrix, ex, ey, sx, sy, x, y), matrix);
 
 
 
