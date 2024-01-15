29d4f6cd4538273f95579c82a8f9930efe135095
ultratavor <felixtonthat64@gmail.com>
2024-01-15 13:28:36 +0100

    ajout d'une fonction pour permettre de récupérer entrée/sortie

--- main/tools.h
+++ main/tools.h
@@ -83,4 +83,66 @@ int get_number_borders(int line, int col){
 }
 
 
+/**
+ * 
+ * @brief récupère les coordonnées de l'entrée et la sortie du mazer 
+ * selon leurs caractères passé en paramètres
+ * 
+ * @param matrix est la matrice d'entrée ou trouver l'entrée et la sortie
+ * @param in est le caractère d'entrée
+ * @param out est le caractère de sortie
+ * @param ex est le paramètre de passage par référence 
+ *  pour les coordonnées de sortie en x
+ * @param ey '' pour les coordonnées de sortie en y
+ * @param sx '' pour les coordonnées de sortie en x
+ * @param sy '' pour les coordonnées de sortie en y
+ * @param heading orientation (passsage par ref'')
+ * 
+ * @return renvoie si les coordonées sont trouvées dans le labyrinthe
+ * 
+*/
+void get_mazer_io(char**matrix, char in, char out, int*ex, int*ey, int*sx, int*sy, int*heading_in, int*heading_out){
+    // on récupère les données d'initialisation/collect data for init
+    int found=0;// pas besoin de continuer le parcours quand on a trouvé les deux coordonnées
+    for (int i=0;i<x&&found!=2;i++){
+        for (int j=0;j<y&&found!=2;j++){
+            if (matrix[i][j] == in){
+                *ex=i;
+                *ey=j;
+                // calcul de l'orientation entrée
+                if (*ex == 0){// we are in north
+                    *heading_in=1;
+                } else if (*ex == x-1){// we are in south
+                    *heading_in=0;
+                } else if (*ey == 0){// we are in est
+                    *heading_in=2;
+                } else if (*ey == y-1){// we are in west
+                    *heading_in=3;
+                }
+                found++;
+            } else if (matrix[i][j] == out){
+                *sx=i;
+                *sy=j;  
+                // calcul de l'orientation (seulement pour le point d'entrée pas de sortie)
+                if (*sx == 0){// we are in north
+                    *heading_out=1;
+                } else if (*sx == x-1){// we are in south
+                    *heading_out=0;
+                } else if (*sy == 0){// we are in est
+                    *heading_out=2;
+                } else if (*sy == y-1){// we are in west
+                    *heading_out=3;
+                } 
+                found++;                             
+            }
+        }
+    }
+    // génération de problème de coordonnées si l'entrée ou la 
+    // sortie n'est pas présente dans la suite du code
+    return found == 2;
+}
 
+
+
+
+// end
