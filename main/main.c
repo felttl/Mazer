bce3b8e5da346d9d3cd9043d46750298d2376feb
ultratavor <felixtonthat64@gmail.com>
2024-01-15 13:28:13 +0100

    programme principal solv incohérence malloc

--- main/main.c
+++ main/main.c
@@ -39,63 +39,48 @@ int main(){
     // on récupère les données d'initialisation/collect data for init
     int ex=0, ey=0;// l'entrée/input
     int sx=0, sy=0;// sortie/output
-    int heading; // orientation
-    for (int i=0;i<x;i++){
-        for (int j=0;j<y;j++){
-            if (matrix[i][j] == '2'){
-                ex=i;
-                ey=j;
-                // calcul de l'orientation (seulement pour le point d'entrée pas de sortie)
-                if (ex == 0){// we are in north
-                    heading=1;
-                } else if (ex == x-1){// we are in south
-                    heading=0;
-                } else if (ey == 0){// we are in est
-                    heading=2;
-                } else if (ey == y-1){// we are in west
-                    heading=3;
-                }
-            } else if (matrix[i][j] == '3'){
-                sx=i;
-                sy=j;                
-            }
-        }
+    int heading_in; // orientation de pour le pion de l'entrée
+    int heading_out; // orientation de pour le pion de la sortie
+    // si on a trouvé les coordonées entree sortie on continue    
+    if (get_mazer_io(matrix, '2', '3', &ex, &ey, &sx, &sy, &heading_in, &heading_out)){
+        printf("matrice en commencant par l'entrée en méthode rotation a droite:\n");
+        // on commence par l'entrée en tournant a droite
+        // affichange déja inclu dans la fonction réccursive  
+        forward_right(&matrix, ex, ey, sx, sy, x, y, heading_in);  
+
+
+        // on charge les même données pour les 3 cas
+        printf("méthode en commençant par la sortie(+ rotation droite)\n");    
+        forward_right(&matrix1, sx, sy, ex, ey, x, y, heading_out);
+
+
+        // on commmence par la sortie avec l'algo de proche en proche
+        Pion*chemin2=add_path(shortest_point_way(matrix2, ex, ey, sx, sy, x, y), matrix2);
+        printf("chemin créé avec la méthode de parcours de proche en proche\n");
+        display(matrix2, x, y);      
+
+        // on utilise l'algo DFS
+        // Pion*cheminDFS=add_path(          (matrix, ex, ey, sx, sy, x, y), matrix2);
+        // printf("chemin créer avec l'algorithme DFS\n");
+        // display(matrix3, x, y);
+
+        // j'enregistre le tout dans le répertoire courant (pour la première méthode)
+        // on se sauvegarde pas les matrices avec les autres algo car c'est le même
+        // resultat
+        // on peut faire la fonction avant ou aprés avoir écrit le chemin de sortie dedans
+        save_matr(matrix, x);    
+
+        // on lache la mémoire  
+        free_pion_chain(chemin2);        
+        //free_pion_chain(cheminDFS);  
+    } else {
+        printf("les coordonnées d'entrée/sortie n'ont pas étés trouvées 
+        ou incohérence des données(ex: plusieurs entrées\n");
     }
-    // on commence cette fois par la sortie
-    printf("matrice en commencant par l'entrée en méthode rotation a droite:\n");
-    // on cherche la sortie et on renvoie le pointeur pour liberer la memoire
-    // on commence par l'entrée en tournant a droite
-    forward_right(&matrix, ex, ey, sx, sy, x, y, heading);    
 
 
-    // on comence par la sortie en tournant a droite
-    printf("méthode en commençant par la sortie(+ rotation droite)\n");    
-    forward_right(&matrix1, sx, sy, ex, ey, x, y, 1);
 
-    // display(matrix1, x, y);    
-
-    // on commmence par la sortie avec l'algo de proche en proche
-    Pion*chemin2=add_path(shortest_point_way(matrix2, ex, ey, sx, sy, x, y), matrix2);
-    printf("chemin créé avec la méthode de parcours de proche en proche\n");
-    display(matrix2, x, y);  
-    
-    // on utilise l'algo DFS
-    // Pion*cheminDFS=add_path(          (matrix, ex, ey, sx, sy, x, y), matrix2);
-    // printf("chemin créer avec l'algorithme DFS\n");
-    // display(matrix3, x, y);        
-
-    // j'enregistre le tout dans le répertoire courant (pour la première méthode)
-    // on se sauvegarde pas les matrices avec les autres algo car c'est le même
-    // resultat
-    // on peut faire la fonction avant ou aprés avoir écrit le chemin de sortie dedans
-    save_matr(matrix, x);
-
-    // on lache la mémoire  
-    free_pion_chain(chemin2);
-    //free_pion_chain(cheminDFS);  
-
-    // pas besoin de léibérer chaque valeur dynamique
-    // matrix1 et 2 déja libéré par les lignes qui suivent:
+    // libération de mémoire
     // REGLE d'or/Golden Rule: 
     // 1 free par 1 malloc/calloc/realloc utilisés
     for (short int i=0; i<x; i++){
